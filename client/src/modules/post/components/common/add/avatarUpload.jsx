ed7909c0cf430e16660964b188b449f4651e6780
taipd-vnist <taipd.vnist@gmail.com>
2021-05-15 14:47:17 +0700

    upload image

--- client/src/modules/post/components/common/add/avatarUpload.jsx
+++ client/src/modules/post/components/common/add/avatarUpload.jsx
@@ -32,6 +32,7 @@ const AvatarUpload = (props) => {
         </div>
 
         <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
             listType="picture-card"
             fileList={avatar}
             onChange={onChange}
--- client/src/modules/post/components/common/add/imagesUpload.jsx
+++ client/src/modules/post/components/common/add/imagesUpload.jsx
@@ -32,6 +32,7 @@ const ImageUpload = (props) => {
         </div>
 
         <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
             listType="picture-card"
             fileList={images}
             onChange={onChange}
